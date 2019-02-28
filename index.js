605fd95320204a1ed29a9c676d5a4e6f8c40ea4f
TaufiqAF <47863034+TaufiqAF@users.noreply.github.com>
2019-03-01 05:53:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -121,10 +121,10 @@ client.on("message", async message => {
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 100);
+    const deleteCount = parseInt(args[0], 10);
     
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 1000)
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
